ce9e65cc073d091698da2bf05a3287bce8b34ca5
phiquangdat <datphi020603@gmail.com>
2025-09-27 20:35:49 +0300

    Refactor App component imports for improved structure
    - Updated import statement in `App.tsx` to use `StrictMode` from React, enhancing the overall component structure and ensuring better adherence to best practices.

--- veridi-os/frontend/src/App.tsx
+++ veridi-os/frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { StrictMode } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "./hooks/useTheme";
